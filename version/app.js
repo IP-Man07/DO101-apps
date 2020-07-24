34d1c6db405c977f1915c266ad301cc18cf31a91
IP-Man07 <shamash07@yahoo.com>
2020-07-24 07:46:17 -0300

    v2 do programa, agora traduzido.

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = 'Agora estamos na versão 2 do programa. Ahora hablamos português!' + '\n';
 
     //send the response to the client
     res.send(response);
